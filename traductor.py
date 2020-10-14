97fdf2e1671eb13ec153723873688f0d27211227
jesdomtri <jesdomtri@gmail.com>
2020-10-14 22:53:20 +0200

    Format code

--- traductor.py
+++ traductor.py
@@ -2,31 +2,37 @@
 # -*- coding: utf-8 -*-
 
 from googletrans import Translator
+import tkinter
 import re
 import time
 
 
 def translate_text_function(line, dst):
     text = line.split('<value>')[1].split('</value>')[0]
-    translatedValue = Translator().translate(text, src='es',dest=dst)
+    translatedValue = Translator().translate(text, src='es', dest=dst)
     value = re.sub(text, translatedValue.text, line)
     return value
+
 
 def loop_files_function(title, languages):
     for language in languages:
         start_time = time.time()
         create_file_function(title, language)
-        print("TIEMPO PARA IDIOMA " + language + ": --- %s seconds ---" % (time.time() - start_time))
+        print("TIEMPO PARA IDIOMA " + language + ": --- %s seconds ---" %
+              (time.time() - start_time))
+
 
 def create_file_function(title, language):
     originalFile = open(title + ".aspx.resx", "r", encoding="utf-8")
-    newFile = open(title + '.aspx.' + language + '.resx', "w+", encoding="utf-8")
+    newFile = open(title + '.aspx.' + language +
+                   '.resx', "w+", encoding="utf-8")
     if 'es' in language:
         for line in originalFile:
             newFile.write(line)
     else:
         translate_file_function(originalFile, newFile, language)
     newFile.close()
+
 
 def translate_file_function(originalFile, newFile, language):
     commentFinished = False
@@ -47,9 +53,11 @@ def translate_file_function(originalFile, newFile, language):
                 dataReached = True
             newFile.write(line)
 
+
 if __name__ == "__main__":
     start_time = time.time()
-    languages = ['es','es-AR','es-cl','es-UY','es-CO','es-ec','es-pa','es-PE','es-VE','en','de','fr','it','pt']
+    languages = ['es', 'es-AR', 'es-cl', 'es-UY', 'es-CO', 'es-ec',
+                 'es-pa', 'es-PE', 'es-VE', 'en', 'de', 'fr', 'it', 'pt']
     try:
         loop_files_function('Default', languages)
     except:
